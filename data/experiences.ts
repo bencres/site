8f7281aa7def74cf1318e674243411c69efbc64b
Ben Cressman <ben@utdallas.edu>
2025-12-26 11:39:01 -0500

    feat: add current position to experiences

--- data/experiences.ts
+++ data/experiences.ts
@@ -1,17 +1,39 @@
 export const experiences = [
   {
+    title: "Pipeline TD",
+    company: "Pixel Foundry",
+    duration: "May 2025 - Present",
+    description:
+      "I am a pipeline TD at Pixel Foundry, where I am responsible for developing our photo-real effects and photogrammetry pipeline. I built our project management and version control system and CI/CD pipeline.",
+    tools: [
+      "Python",
+      "Houdini",
+      "Nuke",
+      "GitHub Actions",
+    ],
+  },
+  {
     title: "VR Developer",
     company: "Multimodal Interaction Lab",
     duration: "Jan 2025 - May 2025",
     description:
       "Responsible for writing software to integrate novel VR haptics hardware with Unity and Meta's XR SDK. Coauthored a paper pending review proposing new VR haptics hardware.",
+    tools: [
+      "C#",
+      "Unity",
+      "Meta XR SDK",
+    ],
   },
   {
     title: "Teaching Assistant",
     company: "The University of Texas at Dallas",
-    duration: "Aug 2023 - Present",
+    duration: "Aug 2023 - May 2026",
     description:
-      "I teach Programming for Digital Artists, a class in which we introduce Python development. I develop Python packages to automate grading and feedback, unit tests for both packages and student code, write technical documentation, lecture, and design assignments.",
+      "Taught Programming for Digital Artists, an introductory Python course. Wrote the Bass School's educational programming infrastructure to automate testing, grading, and feedback, reducing server costs by 16x as compared to the previous system.",
+    tools: [
+      "Python",
+      "GitHub Actions",
+    ],
   },
   {
     title: "Technical Artist",
@@ -19,5 +41,22 @@ export const experiences = [
     duration: "May 2023 - Sep 2023",
     description:
       "I was the technical artist on the production of Grimhook, for which I developed multiple shaders and tools to meet a variety of production needs. These included a real-time ocean using fast Fourier transform and a procedural hand-painting generator.",
+    tools: [
+      "Python",
+      "HLSL",
+      "Unreal Engine",
+    ],
   },
+  {
+    title: "Lead Artist",
+    company: "The University of Texas at Dallas",
+    duration: "January 2023 - May 2023",
+    description:
+      "I was the lead artist on the production of Death Bloom, for which I developed a variety of tools and shaders to meet a variety of production needs. These included a real-time ocean using fast Fourier transform and a procedural hand-painting generator.",
+    tools: [
+      "Python",
+      "HLSL",
+      "Unreal Engine",
+    ],
+  }
 ];
