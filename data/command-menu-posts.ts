33ca1e6bf50a8a31358f6c1641f4b209f3e0e704
Ben Cressman <ben@utdallas.edu>
2026-01-05 11:37:48 -0600

    feat: add recent blog post to featured posts

--- data/command-menu-posts.ts
+++ data/command-menu-posts.ts
@@ -1,5 +1,9 @@
 export const posts = [
   {
+    title: "Universal Asset Browser Progress Update: Plugins, External Libraries, and the Registry Pattern",
+    slug: "006-progress-uab-plugins",
+  },
+  {
     title: "Universal Asset Browser Progress Update: GUI Enhancements, Launcher Shenanigans, and a New Server/Client Model",
     slug: "004-progress-uab-gui-enhancements",
   },
