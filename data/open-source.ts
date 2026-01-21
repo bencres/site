e264f83754804652dca0721ef76d6fef6f1dfb6b
Ben Cressman <ben@utdallas.edu>
2026-01-20 18:20:33 -0600

    Revert "feat: add nginx to hytale modding"
    This reverts commit 2a91e48e9b5b85e7f2b829ae3f2e61861141a88a.

--- data/open-source.ts
+++ data/open-source.ts
@@ -3,7 +3,7 @@ export const openSource = [
     title: "Hytale Modding",
     description:
       "Web developer and core maintainer for Hytale Modding, a community site for documentation and guides for modding Hytale.",
-    tags: ["NextJS", "React", "Tailwind", "nginx"],
+    tags: ["NextJS", "React", "Tailwind"],
     sourcelink: "https://github.com/HytaleModding/site",
     livelink: "https://hytalemodding.dev/",
   },
