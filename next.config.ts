abf977e678e98df93867861a54f95e17c2455c0f
Ben Cressman <ben@utdallas.edu>
2026-02-22 14:07:53 -0600

    feat: add redirect for django + angular demo

--- next.config.ts
+++ next.config.ts
@@ -6,4 +6,16 @@ const nextConfig: NextConfig = {
   },
 };
 
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/demo/django-angular',
+        destination: 'https://github.com/bencres/demo-django-angular',
+        permanent: true,
+      },
+    ];
+  },
+};
+
 export default nextConfig;
